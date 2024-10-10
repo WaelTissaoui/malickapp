155a346d5e7fa330c25158713b05660102feb59d
WaelTissaoui <61127423+WaelTissaoui@users.noreply.github.com>
2024-10-10 07:01:43 +0200

    Add files via upload

---
+++ commentimporter.py
@@ -0,0 +1,38 @@
+import os
+import re
+from py2neo import Graph, Node
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import TokenTextSplitter
+
+# Configurer la connexion à la base de données Neo4j
+graph = Graph("bolt://localhost:7689", auth=("neo4j", "Abdourahmane"))
+
+def importer_pdf_et_creer_noeuds(pdf_path):
+    """Importe le contenu d'un fichier PDF et crée des nœuds dans Neo4j."""
+    try:
+        # Charger le PDF
+        loader = PyPDFLoader(pdf_path)
+        raw_docs = loader.load()
+        
+        # Diviser le contenu en morceaux
+        text_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=24)
+        docs = text_splitter.split_documents(raw_docs)
+        
+        # Ajouter chaque morceau de texte comme nœud dans Neo4j
+        for doc in docs:
+            content = doc.page_content
+            node = Node("Document", content=content)
+            graph.create(node)
+        
+        print("Importation réussie et nœuds créés dans Neo4j.")
+
+    except Exception as e:
+        print(f"Erreur lors de l'importation du PDF : {e}")
+
+# Spécifier le chemin du fichier PDF
+pdf_path = "C:\\Users\\abd_o\\Downloads\\OTBwb3lNdWNWczFzdEk0aUhxRnNOQT09.pdf"
+
+# Appeler la fonction pour importer le PDF et créer des nœuds
+importer_pdf_et_creer_noeuds(pdf_path)
+
+
